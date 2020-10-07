9d100bb4837b17579245f4d49b01507e2eab378c
Burak Tunca <burak.tunca@kizilay.org.tr>
2020-10-07 09:30:38 +0300

    Push 3

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from store.models import Category
+
+class categoryadmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("name",)}
+    list_display = (
+    'pk',
+    'name',
+    'slug',
+    'is_active'
+    )
+    list_filter = ('name','is_active',)
+    list_editable = ('name','slug','is_active',)
+
+
+
+admin.site.register(Category,categoryadmin)
